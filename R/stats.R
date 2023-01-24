393f4d07670095c1ed1e9a16732e2a7fdeddf49c
John Waller <ftw712@ku.dk>
2023-01-24 14:07:58 +0100

    test full script

--- R/stats.R
+++ R/stats.R
@@ -1,7 +1,7 @@
 
 library(dplyr)
 
-# setwd("C:/Users/ftw712/Desktop/private-sector-publishing/")
+setwd("C:/Users/ftw712/Desktop/private-sector-publishing/")
 
 ss = readr::read_tsv("data/source.tsv") %>% 
 mutate(pd = ifelse(grepl("publisher",link),"publisher","dataset")) %>% 
@@ -11,60 +11,62 @@ glimpse()
 
 gbif_country = rgbif::enumeration_country() %>% select(Country=title,iso2) %>% glimpse()
 
-# pp = ss %>%
-# dplyr::filter(pd == "publisher") %>%
-# select(key,`Activity sector`) %>% 
-# mutate(name = map_chr(key,~rgbif::dataset_search(publishingOrg=.x,limit=1)$data$publishingOrganization)) %>% 
-# mutate(`Occurrence records` = map_dbl(key,~ rgbif::occ_search(publishingOrg = .x,limit=0)$meta$count)) %>% 
-# mutate(Datasets = map_dbl(key,~rgbif::dataset_search(publishingOrg= .x,limit=0)$meta$count)) %>% 
-# mutate(`Data citations` = map_dbl(key,~rgbif::lit_count(publishingOrg = .x))) %>%
-# mutate(Company = paste0("https://www.gbif.org/publisher/",key,"[",name,"]")) %>%
-# mutate(iso2 = map_chr(key,~rgbif::dataset_search(publishingOrg=.x,limit=1)$data$publishingCountry)) %>%
-# merge(gbif_country,by="iso2") %>%
-# glimpse()
+pp = ss %>%
+dplyr::filter(pd == "publisher") %>%
+select(key,`Activity sector`) %>% 
+mutate(name = map_chr(key,~rgbif::dataset_search(publishingOrg=.x,limit=1)$data$publishingOrganization)) %>% 
+mutate(`Occurrence records` = map_dbl(key,~ rgbif::occ_search(publishingOrg = .x,limit=0)$meta$count)) %>% 
+mutate(Datasets = map_dbl(key,~rgbif::dataset_search(publishingOrg= .x,limit=0)$meta$count)) %>% 
+mutate(`Data citations` = map_dbl(key,~rgbif::lit_count(publishingOrg = .x))) %>%
+mutate(Company = paste0("https://www.gbif.org/publisher/",key,"[",name,"]")) %>%
+mutate(iso2 = map_chr(key,~rgbif::dataset_search(publishingOrg=.x,limit=1)$data$publishingCountry)) %>%
+merge(gbif_country,by="iso2") %>%
+glimpse()
 
-# dd = ss %>% 
-# dplyr::filter(pd == "dataset") %>%
-# select(key,`Activity sector`) %>% 
-# mutate(name = map_chr(key,~rgbif::datasets(uuid=.x,limit=1)$data$title)) %>%
-# mutate(`Occurrence records` = map_dbl(key,~
-# rgbif::occ_search(datasetKey = .x,limit=0)$meta$count)) %>%
-# mutate(Datasets = 1) %>% 
-# mutate(`Data citations` = map_dbl(key,~rgbif::lit_count(datasetKey = .x))) %>%
-# mutate(Company = paste0("[",name,"](https://www.gbif.org/dataset/",key,")"))%>% 
-# mutate(p_key = map_chr(key,~ rgbif::datasets(uuid=.x,limit=1)$data$publishingOrganizationKey)) %>%
-# mutate(iso2 = map_chr(p_key,~rgbif::dataset_search(publishingOrg=.x,limit=1)$data$publishingCountry)) %>%
-# merge(gbif_country,by="iso2") %>%
-# select(-p_key) %>%
-# glimpse()
+dd = ss %>% 
+dplyr::filter(pd == "dataset") %>%
+select(key,`Activity sector`) %>% 
+mutate(name = map_chr(key,~rgbif::datasets(uuid=.x,limit=1)$data$title)) %>%
+mutate(`Occurrence records` = map_dbl(key,~
+rgbif::occ_search(datasetKey = .x,limit=0)$meta$count)) %>%
+mutate(Datasets = 1) %>% 
+mutate(`Data citations` = map_dbl(key,~rgbif::lit_count(datasetKey = .x))) %>%
+mutate(Company = paste0("[",name,"](https://www.gbif.org/dataset/",key,")"))%>% 
+mutate(p_key = map_chr(key,~ rgbif::datasets(uuid=.x,limit=1)$data$publishingOrganizationKey)) %>%
+mutate(iso2 = map_chr(p_key,~rgbif::dataset_search(publishingOrg=.x,limit=1)$data$publishingCountry)) %>%
+merge(gbif_country,by="iso2") %>%
+select(-p_key) %>%
+glimpse()
 
-# tt = rbind(pp,dd) %>% 
-# arrange(name) %>%
-# select(Company, `Activity sector`,	Country, Datasets, `Occurrence records`, `Data citations`) %>%
-# glimpse() 
+tt = rbind(pp,dd) %>% 
+arrange(name) %>%
+select(Company, `Activity sector`,	Country, Datasets, `Occurrence records`, `Data citations`) %>%
+glimpse() 
 
-# save_file_table = paste0("exports/table-",Sys.Date(),".adoc")
+save_file_table = paste0("exports/table-",Sys.Date(),".adoc")
 
-# sink(file = save_file_table, type = "output")
-# tt %>%  
-# mutate(`Datasets` = format(`Datasets`, nsmall=0, big.mark=",")) %>%
-# mutate(`Occurrence records` = format(`Occurrence records`, nsmall=0, big.mark=",")) %>%
-# mutate(`Data citations` = format(`Data citations`, nsmall=0, big.mark=",")) %>%
-# ascii::ascii(include.rownames = FALSE, digits = 0)
-# sink()
+sink(file = save_file_table, type = "output")
+tt %>%  
+mutate(`Datasets` = format(`Datasets`, nsmall=0, big.mark=",")) %>%
+mutate(`Occurrence records` = format(`Occurrence records`, nsmall=0, big.mark=",")) %>%
+mutate(`Data citations` = format(`Data citations`, nsmall=0, big.mark=",")) %>%
+ascii::ascii(include.rownames = FALSE, digits = 0)
+sink()
 
 # totals table
-# save_file_totals = paste0("exports/table-totals-",Sys.Date(),".adoc")
+save_file_totals = paste0("exports/table-totals-",Sys.Date(),".adoc")
 
-# sink(file = save_file_totals, type = "output")
-# tt %>% 
-# summarise(
-# Datasets = sum(Datasets),
-# `Occurrence records` = sum(`Occurrence records`), 
-# `Data citations` = sum(`Data citations`)
-# ) %>%
-# mutate(`Datasets` = format(`Datasets`, nsmall=0, big.mark=",")) %>%
-# mutate(`Occurrence records` = format(`Occurrence records`, nsmall=0, big.mark=",")) %>%
-# mutate(`Data citations` = format(`Data citations`, nsmall=0, big.mark=",")) %>%
-# ascii::ascii(include.rownames = FALSE, digits = 0)
-# sink()
+sink(file = save_file_totals, type = "output")
+tt %>% 
+summarise(
+Datasets = sum(Datasets),
+`Occurrence records` = sum(`Occurrence records`), 
+`Data citations` = sum(`Data citations`)
+) %>%
+mutate(`Datasets` = format(`Datasets`, nsmall=0, big.mark=",")) %>%
+mutate(`Occurrence records` = format(`Occurrence records`, nsmall=0, big.mark=",")) %>%
+mutate(`Data citations` = format(`Data citations`, nsmall=0, big.mark=",")) %>%
+ascii::ascii(include.rownames = FALSE, digits = 0)
+sink()
+
+list.files("exports/")
